804692698b2538d9062c8b72db88f697adef9715
Andres Bonilla <andres94b@gmail.com>
2025-01-29 09:00:24 -0500

    contact buttons

--- scripts/contact.js
+++ scripts/contact.js
@@ -8,4 +8,22 @@ document.addEventListener('DOMContentLoaded', () => {
             alert('success');
         });
     }
+    
+
+    const callCard = document.getElementById('call-card');
+    const emailCard = document.getElementById('email-card');
+    const messageCart = document.getElementById('text-card');
+    
+    callCard.addEventListener('click',()=>{
+        window.open("tel:4383659614");
+    });
+
+    emailCard.addEventListener('click',()=>{
+        window.open("mailto:andres94b@gmail.com");
+    });
+
+    messageCart.addEventListener('click',()=>{
+        window.open("https://wa.me/14383659614");
+    });
+    
 });
